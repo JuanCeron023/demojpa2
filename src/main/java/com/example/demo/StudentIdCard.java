302a9e5eea4ed10e731aecf7fe2fc491355ae5cd
JuanCeron023 <juanceron256@gmail.com>
2022-01-07 14:59:24 -0500

    cambios v1

---
+++ src/main/java/com/example/demo/StudentIdCard.java
@@ -0,0 +1,81 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "StudentIdCard")
+@Table(
+        name = "student_id_card",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "student_id_card_number_unique",
+                        columnNames = "card_number"
+                )
+        }
+)
+public class StudentIdCard {
+
+    @Id
+    @SequenceGenerator(
+            name = "algo1",
+            sequenceName = "algo2",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "algo2"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "card_number",
+            nullable = false,
+            length = 15
+    )
+    private String cardNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "student_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "student_id_fk"
+            )
+    )
+    private Student student;
+
+    public StudentIdCard(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public StudentIdCard(String cardNumber, Student student) {
+        this.cardNumber = cardNumber;
+        this.student = student;
+    }
+
+    public StudentIdCard() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentIdCard{" +
+                "id=" + id +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", student=" + student +
+                '}';
+    }
+
+}
